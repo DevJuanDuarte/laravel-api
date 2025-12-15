716a4feb38cee57c4143a47e0e5df9a365cddcdc
Juan Duarte <juan.devcontact@gmail.com>
2025-12-14 23:05:41 -0500

    fix: include environment configuration in root route response

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return ['Laravel' => app()->version(), 'env' => config('app.env')];
 });
 
